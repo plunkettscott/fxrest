da500d0f8427fac710bb988415c53980ddca1722
Scott Plunkett <scottplunkett15@gmail.com>
2017-07-15 20:14:31 -0500

    More changes. Undocumented. Config file updated.

--- config.lua
+++ config.lua
@@ -1,12 +1,11 @@
 
 RESTConfig                  = {}
-RESTConfig.Host             = "http://lesa5.dev"
+RESTConfig.Host             = "http://localhost"
 RESTConfig.APIPath          = "/api/v1/" -- Ensure you have your trailing slash. It is not checked for.
 
 RESTConfig.Headers          = {
                                 ['Content-Type'] = 'application/json',
-                                ['Accept'] = 'application/json',
-                                ['Authorization'] = 'Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiIsImp0aSI6ImUwZTRkM2FmN2U4ZDk0Y2UwNTc1MWE5ZWZmYTM4YmViYWEwM2I1N2U0NjM2OGM2NzIwN2FkOWNkZGEyNGYxYmE3ZmYyYWE2OGI4YmY1ZTNmIn0.eyJhdWQiOiIxIiwianRpIjoiZTBlNGQzYWY3ZThkOTRjZTA1NzUxYTllZmZhMzhiZWJhYTAzYjU3ZTQ2MzY4YzY3MjA3YWQ5Y2RkYTI0ZjFiYTdmZjJhYTY4YjhiZjVlM2YiLCJpYXQiOjE0OTk3MDU5NjYsIm5iZiI6MTQ5OTcwNTk2NiwiZXhwIjoxNTMxMjQxOTY1LCJzdWIiOiIxIiwic2NvcGVzIjpbXX0.EWIfCZ9yTrHYa2mJWU5jVknCY2K7ZVI6Md0nFtaYGRT9vujOXdSyf4dQzegWK6Lx1_UDJPAY4UKQrQl88r6n4p1QojqzYQoPo6KLd7W75e4biWxyCqZXbz2tCES95ahWJsx4kh4YOLFE_1cyddC18sO99AdtQmSTDVzU72YFmrWIdYLxHrssqY5b2Fnr0mU0fnG8X-AWZGcYcnrn2j5kdHYDPw-rqXdvPrf789Xro_D0wPVsZKxEMU2K30WToyXz4qbX7EPA387BO8SkFyRqbeHfjj51VCtMrIjt_1_jyftVvVoLklzJFLVXM3nEjQvpafE220OBtC8i917qJ7xi0-VmXBYN6Bo8jpZfdjdlWIP0g46LZAYFxzbSqpv8b51jHvQ9gp64BixrG2e50h-vPXpXE86AHxTsshXRPHgAS9GFx8dNhcl3jzj0woTd1cX7px1Z4bV_QZP-hPsKjP_gnebTesfE2IzNqoH1AaXTScoHto_Z3ckBNeIUd658pXuzDlfDW6YBCrqTT4IryS0vmjoLl3amnRkWQmMLKRfs0TGeFJFbCQMUyEVSRGMf5_UaJD3by__onpg7Su7E30V0-sW-2Y3VjDzgzk2e5YyS9_x9p9_L4HhRoJAKV5IvV1LVnTJ47IiPK-TyxlZiqVaHelt5hhRXH6erQ9nigdxEzfk'
+                                ['Accept'] = 'application/json'
                               }
 
 RESTConfig.SuccessCodes     = {
@@ -18,4 +17,7 @@ RESTConfig.SuccessCodes     = {
                               }
 
 
-RESTConfig.DefSaveMethod    = 'POST'                            
+RESTConfig.DefSaveMethod    = 'POST'
+RESTConfig.DefCreateMethod  = 'POST'
+RESTConfig.DefDeleteMethod  = 'POST'
+RESTConfig.DefUpdateMethod  = 'POST'
--- rest.lua
+++ rest.lua
@@ -1,4 +1,5 @@
 
+
 MODEL                   = {}
 MODEL.__index           = MODEL
 MODEL.Storage           = {}
@@ -8,15 +9,40 @@ MODEL.FromResource      = function(self, source, name)
                             MODEL.Storage[source][name] = {
                               queryString = self.queryString,
                               path = self.path,
-                              props = {},
                               data = {},
                               vars = {},
                               isSaved = false
                             }
+                            self.source = source
+                            self.name = name
+                            self.model = MODEL.Storage[source][name]
+                            self.props = {}
                             REST.DirectGet(self.path, self.queryString, function(data)
                               MODEL.UpdateData(source, name, data)
                             end)
-                            print(' :: FXRest -> Action: Store -> Model: ' .. name .. ' | Source: #' .. source .. ' | Status: Successful - OK')
+                            print(' :: FXRest -> Action: From Resource -> Model: ' .. name .. ' | Source: #' .. source .. ' | Status: Successful - 200')
+                            return self
+                          end
+
+MODEL.CreateFromResource = function(self, source, name, data, method)
+                            if method == nil then
+                              method = RESTConfig.DefCreateMethod
+                            end
+                            MODEL.Storage[source] = {}
+                            MODEL.Storage[source][name] = {
+                              queryString = self.queryString,
+                              path = self.path,
+                              data = {},
+                              vars = {},
+                              isSaved = false
+                            }
+                            self.source = source
+                            self.name = name
+                            self.model = MODEL.Storage[source][name]
+                            self.props =
+                            MODEL.Create(self, data, method)
+                            print(' :: FXRest -> Action: New Resource -> Model: ' .. name .. ' | Source: #' .. source .. ' | Status: Successful - 200')
+                            return self
                           end
 
 MODEL.UpdateData        = function(source, name, data)
@@ -90,8 +116,48 @@ MODEL.Save              = function(self, method)
                             return self
                           end
 
+MODEL.Create            = function(self, data, method)
+                            MODEL.Storage[self.source][self.name].isSaved = false
+                            if method == nil then
+                              method = RESTConfig.DefCreateMethod
+                            end
+                            PerformHttpRequest(
+                              self.model.path,
+                              function(code, text, headers)
+                                if RESTHelper.IsSuccess(code) then
+                                  REST.DirectGet(self.model.path, self.model.queryString, function(data)
+                                    MODEL.UpdateData(self.source, self.name, data)
+                                  end)
+                                  print(' :: FXRest -> Action: Create -> Model: ' .. self.name .. ' | Source: #' .. self.source .. ' | Status: Successful - ' .. code)
+                                else
+                                  print(' :: FXRest -> Action: Create -> Model: ' .. self.name .. ' | Source: #' .. self.source .. ' | Status: Failure - ' .. code)
+                                end
+                              end,
+                              method,
+                              json.encode(data),
+                              RESTConfig.Headers
+                            )
+                            self.props = {}
+                            return self
+                          end
+
+MODEL.Refresh           = function(self)
+                            MODEL.Storage[self.source][self.name].isSaved = false
+                            REST.DirectGet(self.model.path, self.model.queryString, function(data)
+                              MODEL.UpdateData(self.source, self.name, data)
+                            end)
+                            return self
+                          end
+
 MODEL.HasSaved          = function(self)
                             return MODEL.Storage[self.source][self.name].isSaved
+                          end
+
+MODEL.WhenSaved         = function(self)
+                            while not MODEL.HasSaved(self) do
+                              Citizen.Wait(100)
+                            end
+                            return self
                           end
 
 MODEL.Data              = function(self)
@@ -127,8 +193,137 @@ REST.One                = function(self, key)
                             return self
                           end
 
-REST.Get                = function(self)
-                            print(self.path)
+REST.Prop              = function(self, key, value)
+                            self.props[key] = value
+                            return self
+                          end
+
+REST.Props             = function(self, params)
+                            for key,value in pairs(params) do
+                              self.props[key] = value
+                            end
+                            return self
+                          end
+
+REST.Get                = function(self, callback, query)
+                            if query == nil then
+                              query = '?'
+                            end
+                            PerformHttpRequest(
+                              self.path,
+                              function(code, text, headers)
+                                if RESTHelper.IsSuccess(code) then
+                                  print(' :: FXRest -> Action: GET | Path: ' .. self.path .. ' | Status: Successful - ' .. code)
+                                  callback(true, json.decode(text), headers)
+                                else
+                                  print(' :: FXRest -> Action: GET | Path: ' .. self.path .. ' | Status: Failure - ' .. code)
+                                  callback(false, code, headers)
+                                end
+                              end,
+                              'GET',
+                              "",
+                              RESTConfig.Headers
+                            )
+                          end
+
+REST.Update             = function(self, callback, data, method)
+                            if data == nil then
+                              data = self.props
+                            else
+                              for k,v in pairs(data) do
+                                self.props[k] = v
+                              end
+                              data = self.props
+                            end
+                            if method == nil then
+                              method = RESTConfig.DefUpdateMethod
+                            end
+                            PerformHttpRequest(
+                              self.path,
+                              function(code, text, headers)
+                                if RESTHelper.IsSuccess(code) then
+                                  print(' :: FXRest -> Action: Update | Path: ' .. self.path .. ' | Status: Successful - ' .. code)
+                                  callback(true, json.decode(text), headers)
+                                else
+                                  print(' :: FXRest -> Action: Update | Path: ' .. self.path .. ' | Status: Failure - ' .. code)
+                                  callback(false, code, headers)
+                                end
+                              end,
+                              method,
+                              json.encode(data),
+                              RESTConfig.Headers
+                            )
+                            self.props = {}
+                            return self
+                          end
+
+REST.Delete             = function(self, callback, data, method)
+                            if data == nil then
+                              data = self.props
+                            else
+                              for k,v in pairs(data) do
+                                self.props[k] = v
+                              end
+                              data = self.props
+                            end
+                            if method == nil then
+                              method = RESTConfig.DefDeleteMethod
+                            end
+                            PerformHttpRequest(
+                              self.path,
+                              function(code, text, headers)
+                                if RESTHelper.IsSuccess(code) then
+                                  print(' :: FXRest -> Action: Delete | Path: ' .. self.path .. ' | Status: Successful - ' .. code)
+                                  callback(true, json.decode(text), headers)
+                                else
+                                  print(' :: FXRest -> Action: Delete | Path: ' .. self.path .. ' | Status: Failure - ' .. code)
+                                  callback(false, code, headers)
+                                end
+                              end,
+                              method,
+                              json.encode(data),
+                              RESTConfig.Headers
+                            )
+                            self.props = {}
+                            return self
+                          end
+
+REST.Create             = function(self, callback, data, method)
+                            print('running...')
+                            if data == nil then
+                              data = self.props
+                              print('props broadcasted.')
+                            else
+                              for k,v in pairs(data) do
+                                print('setting data ' .. k .. ' to ' .. v)
+                                self.props[k] = v
+                              end
+                              data = self.props
+                              print('set')
+                            end
+                            if method == nil then
+                              method = RESTConfig.DefCreateMethod
+                              print('method set')
+                            end
+                            print('http go')
+                            PerformHttpRequest(
+                              self.path,
+                              function(code, text, headers)
+                                if RESTHelper.IsSuccess(code) then
+                                  print(' :: FXRest -> Action: Create | Path: ' .. self.path .. ' | Status: Successful - ' .. code)
+                                  callback(true, json.decode(text), headers)
+                                else
+                                  print(' :: FXRest -> Action: Create | Path: ' .. self.path .. ' | Status: Failure - ' .. code)
+                                  callback(false, code, headers)
+                                end
+                              end,
+                              method,
+                              json.encode(data),
+                              RESTConfig.Headers
+                            )
+                            print('http was executed. running async like.')
+                            self.props = {}
+                            return self
                           end
 
 REST.DirectGet          = function(path, query, callback)
